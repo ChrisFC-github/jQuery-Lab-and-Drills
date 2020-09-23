ea4a068f825b7baac53c592a88850f7323142b5c
ChrisFC-github <70590048+ChrisFC-github@users.noreply.github.com>
2020-09-23 01:19:38 -0500

    first commit

---
+++ jQuery LAB/jQuery/jQueryLAB.js
@@ -0,0 +1,61 @@
+$(document).ready(function() { // 1
+    var popupButton = $('<li><input type="button" id="alertbtnSubmit" value="Click for an Alert pop up"/></li>');
+    $(popupButton).insertBefore("form");
+    $("#alertbtnSubmit").click(function () {
+        alert("This is a pretty (nice) message");
+    });
+});
+
+$(function () {
+    $("form").submit(function (e) {
+        e.preventDefault();
+        let textValue = $("input[type='text']").val();
+        alert(textValue);
+    });
+});
+
+$(document).ready(function(){
+    $(".hover").hover(function(){
+      $(this).css("background-color", "yellow");
+        }, function(){
+        $(this).css("background-color", "white");
+    });
+});
+
+$('.paragraph').click(function () {
+    let randomNumber1 = Math.floor(Math.random() * 255);
+    let randomNumber2 = Math.floor(Math.random() * 255);
+    let randomNumber3 = Math.floor(Math.random() * 255);
+    $(this).css("color", `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`)
+});
+
+var a = 0;
+
+$(document).ready(function() { // 5
+    var popupButton1 = $('<li><input type="button" id="spanner" value="Click to show a name in a SPAN"/></li>');
+    $(popupButton1).insertAfter("p");
+    $("#spanner").click(function () {
+        a++;
+        if (a < 2) {
+        var nameSpanner = $('<span> Chris C</span>');
+        $(nameSpanner).insertAfter("#spanner");
+        } else {
+
+        }
+    });
+});
+
+const friendsArray = ["Erick H", "Selina F", "Fred S"];
+var b = 0;
+
+$("#btnFriends").click(function () {
+    let friendsText = friendsArray[b];
+    let li = $("<li></li>");
+    if (b < friendsArray.length) {
+    li.append(friendsText);
+    $("ul").append(li);
+    b++;
+    } else {
+
+    }
+});
